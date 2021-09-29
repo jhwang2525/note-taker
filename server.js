af9aec74891f273f5f1ed5432610331c1bf58704
Jun Hwang <jhwang2525@gmail.com>
2021-09-28 21:34:29 -0700

    note-taker

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT || 3001;
 
 let notesData = [];
 
@@ -86,16 +86,16 @@ app.delete("/api/notes/:id", function(req, res) {
 
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/api/notes", function(req, res) {
-  return res.sendFile(path.json(__dirname, "Develop/db/db.json"));
+  return res.sendFile(path.json(__dirname, "db/db.json"));
 });
 
 
